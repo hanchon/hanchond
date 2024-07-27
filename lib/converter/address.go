725ebc064e0b3cde0c3c97166ded3697429d9199
hanchon <guillermo.paoletti@gmail.com>
2024-07-27 10:53:45 +0100

    chore: update docs links and refactor converter code

--- lib/converter/address.go
+++ lib/converter/address.go
@@ -1,11 +1,14 @@
 package converter
 
-import "strings"
-
 // RemoveHexPrefixFromAddress returns a new string without the 0x prefix, does not modify the original string
 func RemoveHexPrefixFromAddress(address string) string {
-	if strings.HasPrefix(address, "0x") {
+	if Has0xPrefix(address) {
 		return address[2:]
 	}
 	return address
+}
+
+// Has0xPrefix from go-ethereum (copied here because it is not exported)
+func Has0xPrefix(str string) bool {
+	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
 }
