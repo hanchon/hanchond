6ee1d22d119247a9a6bdb7d278d31f23bd56e198
hanchon <guillermo.paoletti@gmail.com>
2024-07-28 19:33:33 +0100

    feat: cli to convert numbers

---
+++ cmd/converter/number.go
@@ -0,0 +1,30 @@
+package converter
+
+import (
+	"fmt"
+	"strings"
+
+	converterutil "github.com/hanchon/hanchond/lib/converter"
+	"github.com/spf13/cobra"
+)
+
+// numberCmd represents the number command
+var numberCmd = &cobra.Command{
+	Use:   "number",
+	Args:  cobra.ExactArgs(1),
+	Short: "Convert numbers between decimal and hex",
+	Long:  `No flags required, but hex numbers MUST have the 0x prefix.`,
+	Run: func(_ *cobra.Command, args []string) {
+		number := args[0]
+		input := strings.TrimSpace(number)
+		if converterutil.Has0xPrefix(input) {
+			fmt.Println(converterutil.HexStringToDecimal(input))
+		} else {
+			fmt.Println(converterutil.DecimalStringToHex(input))
+		}
+	},
+}
+
+func init() {
+	ConverterCmd.AddCommand(numberCmd)
+}
