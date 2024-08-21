5c8cfcffa9de6b19b53b9141948316d464a3d558
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2024-08-22 00:29:07 +0200

    feat: UniswapV2Router deployment (#35)
    * feat: uniswapv2router deployment
    
    * docs: add commands docs

--- playground/solidity/deps.go
+++ playground/solidity/deps.go
@@ -21,3 +21,15 @@ func DownloadDep(repoURL, version, name string) (string, error) {
 
 	return path, nil
 }
+
+func DownloadUniswapV2Core() (string, error) {
+	return DownloadDep("https://github.com/Uniswap/uniswap-v2-core", "master", "uniswapv2")
+}
+
+func DownloadUniswapV2Periphery() (string, error) {
+	return DownloadDep("https://github.com/Uniswap/v2-periphery", "master", "v2-periphery")
+}
+
+func DownloadUniswapV2Minified() (string, error) {
+	return DownloadDep("https://github.com/casweeney/minified-uniswapv2-contracts", "main", "v2-minified")
+}
