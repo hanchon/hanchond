4d97f48a9a08bacf6774ead2ec1c96893455da76
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2024-09-12 17:11:45 +0200

    fix: use the correct sqlite driver with no CGO dep (#66)

--- playground/explorer/database/database.go
+++ playground/explorer/database/database.go
@@ -13,7 +13,7 @@ import (
 var ddl string
 
 func InitExplorerDatabase(ctx context.Context, nodeDataPath string) (*sql.DB, *Queries, error) {
-	db, err := sql.Open("sqlite3", nodeDataPath)
+	db, err := sql.Open("sqlite", nodeDataPath)
 	if err != nil {
 		return nil, nil, err
 	}
