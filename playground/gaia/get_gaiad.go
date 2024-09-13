e62bcba1e70e75dfea884cfd51d3a75f31d1b50f
Blackvote <138195264+Blackvote@users.noreply.github.com>
2024-09-13 19:46:28 +0500

    fix: update get_gaiad.go - add arch check

--- playground/gaia/get_gaiad.go
+++ playground/gaia/get_gaiad.go
@@ -1,4 +1,4 @@
-package gaia
+package main
 
 import (
 	"fmt"
@@ -6,22 +6,36 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"runtime"
 )
 
+func main() {
+	err := GetGaiadBinary(false, "v19.2.0")
+	if err != nil {
+		return
+	}
+}
+
 func GetGaiadBinary(isDarwin bool, version string) error {
-	darwinURL := "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-darwin-arm64"
-	url := " https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-linux-amd64"
+	url := "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-linux-amd64"
+	arch := runtime.GOARCH
 	if isDarwin {
-		url = darwinURL
+		if arch == "arm64" {
+			url = "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-darwin-arm64"
+		} else {
+			url = "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-darwin-amd64"
+		}
+	} else {
+		if arch == "arm64" {
+			url = "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-linux-arm64"
+		} else {
+			url = "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-linux-amd64"
+		}
 	}
 	path := filesmanager.GetGaiadPath()
-	//cmdString := fmt.Sprintf("wget %s -O %s && chmod +x %s", url, path, path)
-	//command := exec.Command("bash", "-c", cmdString)
-	//_, err := command.CombinedOutput()
-
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("failed to download Gaia: %v", err)
+		return fmt.Errorf("failed to download Gaia: %s", err)
 	}
 	defer resp.Body.Close()
 
@@ -31,18 +45,18 @@ func GetGaiadBinary(isDarwin bool, version string) error {
 
 	outFile, err := os.Create(path)
 	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
+		return fmt.Errorf("failed to create file: %s", err)
 	}
 	defer outFile.Close()
 
 	_, err = io.Copy(outFile, resp.Body)
 	if err != nil {
-		return fmt.Errorf("failed to save gaiad binary: %w", err)
+		return fmt.Errorf("failed to save gaiad binary: %s", err)
 	}
 
 	err = os.Chmod(path, 0755)
 	if err != nil {
-		return fmt.Errorf("failed to set file permissions: %v", err)
+		return fmt.Errorf("failed to set file permissions: %s", err)
 	}
 
 	return nil
