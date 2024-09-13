2e651b9802035c9746f85144ec2cbc52600c42d7
Blackvote <138195264+Blackvote@users.noreply.github.com>
2024-09-13 19:30:08 +0500

    feat: update get_gaiad.go remove wget add http.Get, io.Copy and os.Chmod

--- playground/gaia/get_gaiad.go
+++ playground/gaia/get_gaiad.go
@@ -2,9 +2,10 @@ package gaia
 
 import (
 	"fmt"
-	"os/exec"
-
 	"github.com/hanchon/hanchond/playground/filesmanager"
+	"io"
+	"net/http"
+	"os"
 )
 
 func GetGaiadBinary(isDarwin bool, version string) error {
@@ -14,8 +15,35 @@ func GetGaiadBinary(isDarwin bool, version string) error {
 		url = darwinURL
 	}
 	path := filesmanager.GetGaiadPath()
-	cmdString := fmt.Sprintf("wget %s -O %s && chmod +x %s", url, path, path)
-	command := exec.Command("bash", "-c", cmdString)
-	_, err := command.CombinedOutput()
-	return err
+	//cmdString := fmt.Sprintf("wget %s -O %s && chmod +x %s", url, path, path)
+	//command := exec.Command("bash", "-c", cmdString)
+	//_, err := command.CombinedOutput()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download Gaia: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download gaiad binary: status code %d", resp.StatusCode)
+	}
+
+	outFile, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save gaiad binary: %w", err)
+	}
+
+	err = os.Chmod(path, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to set file permissions: %v", err)
+	}
+
+	return nil
 }
