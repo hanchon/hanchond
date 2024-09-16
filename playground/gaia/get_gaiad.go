83e78f62ee004a6f547be7111e1907ea118b6d57
Blackvote <138195264+Blackvote@users.noreply.github.com>
2024-09-16 22:44:58 +0500

    refactor: remove wget dependency (#69)
    * feat: update get_gaiad.go remove wget add http.Get, io.Copy and os.Chmod
    
    * fix: update get_gaiad.go - add arch check
    
    * miss package name
    
    * fix: linter issues
    
    ---------
    
    Co-authored-by: hanchon <guillermo.paoletti@gmail.com>

--- playground/gaia/get_gaiad.go
+++ playground/gaia/get_gaiad.go
@@ -2,20 +2,58 @@ package gaia
 
 import (
 	"fmt"
-	"os/exec"
+	"io"
+	"net/http"
+	"os"
+	"runtime"
 
 	"github.com/hanchon/hanchond/playground/filesmanager"
 )
 
 func GetGaiadBinary(isDarwin bool, version string) error {
-	darwinURL := "https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-darwin-arm64"
-	url := " https://github.com/cosmos/gaia/releases/download/" + version + "/gaiad-" + version + "-linux-amd64"
-	if isDarwin {
-		url = darwinURL
+	arch := runtime.GOARCH
+	if arch != "arm64" {
+		arch = "amd64"
 	}
+	systemOS := "darwin"
+	if !isDarwin {
+		systemOS = "linux"
+	}
+
+	url := fmt.Sprintf("https://github.com/cosmos/gaia/releases/download/%s/gaiad-%s-%s-%s", version, version, systemOS, arch)
+
 	path := filesmanager.GetGaiadPath()
-	cmdString := fmt.Sprintf("wget %s -O %s && chmod +x %s", url, path, path)
-	command := exec.Command("bash", "-c", cmdString)
-	_, err := command.CombinedOutput()
-	return err
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to download Gaia: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download gaiad binary: status code %d", resp.StatusCode)
+	}
+
+	outFile, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %s", err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save gaiad binary: %s", err)
+	}
+
+	err = os.Chmod(path, 0o755)
+	if err != nil {
+		return fmt.Errorf("failed to set file permissions: %s", err)
+	}
+
+	return nil
 }
